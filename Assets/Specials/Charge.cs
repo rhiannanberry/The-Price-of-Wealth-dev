166e294db368861c0db828def41655821f785c92
TASallin <tristansallin@yahoo.com>
2019-02-01 00:33:30 -0500

    Fixed random button on character select screen

--- Assets/Specials/Charge.cs
+++ Assets/Specials/Charge.cs
@@ -1,20 +1,24 @@
 public class Charge : Special {
 	
-	public Charge () {name = "Charge"; description = "Attack the front 2 enemies"; baseCost = 3; modifier = 0;}
+	public Charge () {name = "Charge"; description = "Attack the front enemy with high power and other enemies with low power";
+    	baseCost = 3; modifier = 0;}
 	
 	public override TimedMethod[] Use () {
 		if (Attacks.EvasionCheck(Party.GetEnemy(), Party.GetPlayer().GetAccuracy()) && Party.enemyCount > 1) {
-			Character second = Party.enemies[0];
-			for (int i = 1; i < 4; i++) {
-				if (Party.enemies[i] != null && Party.enemies[i].GetAlive()) {
-					second = Party.enemies[i];
-					break;
+			//Character second = Party.enemies[0];
+			TimedMethod[] moves = new TimedMethod[Party.enemyCount];
+			int index = 0;
+			for (int i = 0; i < 4; i++) {
+				if (i != Party.enemySlot - 1 && Party.enemies[i] != null && Party.enemies[i].GetAlive()) {
+					moves[index] = new TimedMethod(0, "AttackAny", new object[] {Party.GetPlayer(), Party.enemies[i],
+			    	Party.GetPlayer().GetStrength()/2, (Party.GetPlayer().GetStrength() + 5)/2, Party.GetPlayer().GetAccuracy(), true, true, false});
+					//break;
 				}
+				index++;
 			}
-		    return new TimedMethod[] {new TimedMethod(60, "StagnantAttack", new object[] {
-		        true, Party.GetPlayer().GetStrength(), Party.GetPlayer().GetStrength() + 5, Party.GetPlayer().GetAccuracy(), true, false, false}),
-			    new TimedMethod(60, "AttackAny", new object[] {Party.GetPlayer(), second, Party.GetPlayer().GetStrength() / 2,
-			    (Party.GetPlayer().GetStrength() + 5)/2, Party.GetPlayer().GetAccuracy(), true, true, false})};
+			moves[index] = new TimedMethod(60, "StagnantAttack", new object[] {
+		        true, Party.GetPlayer().GetStrength(), Party.GetPlayer().GetStrength() + 5, Party.GetPlayer().GetAccuracy(), true, false, false});
+		    return moves;
 		} else {
 			return new TimedMethod[] {new TimedMethod(60, "StagnantAttack", new object[] {
 		        true, Party.GetPlayer().GetStrength(), Party.GetPlayer().GetStrength() + 5, Party.GetPlayer().GetAccuracy(), true, false, false})};
