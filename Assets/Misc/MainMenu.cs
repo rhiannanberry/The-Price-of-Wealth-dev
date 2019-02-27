1ddbe579e745dd6ed24ea292f12227100aa2b676
TASallin <tristansallin@yahoo.com>
2019-02-27 11:58:42 -0500

    Added win menu and settings/credits

---
+++ Assets/Misc/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+    
+	public GameObject start;
+	public GameObject credits;
+	public GameObject settings;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void OpenCredits () {
+		credits.SetActive(true);
+		start.SetActive(false);
+	}
+	 
+	public void OpenSettings () {
+		settings.SetActive(true);
+		start.SetActive(false);
+	}
+	
+	public void ToMain () {
+		settings.SetActive(false);
+		credits.SetActive(false);
+		start.SetActive(true);
+	}
+}
---
+++ Assets/Overworld/Timekeeper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timekeeper : MonoBehaviour {
+    
+	public GameObject sun;
+	
+	// Use this for initialization
+	void Start () {
+		sun.transform.localPosition = new Vector3(System.Math.Min(Time.timeUnit, 100) - 400, sun.transform.localPosition.y, sun.transform.localPosition.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+}
